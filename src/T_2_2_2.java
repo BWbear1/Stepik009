e8d037c57360c19270471c2a742b07f4ce854a33
BWbear1 <blackwhitebear1@gmail.com>
2023-09-23 14:42:13 +0200

    T_2_2_2

---
+++ src/T_2_2_2.java
@@ -0,0 +1,11 @@
+public class T_2_2_2 {
+    public static void main(String[] args) {
+    }
+
+    char c = '\\';
+    int charValue = c;
+    int charHuechar = charValue+a;
+    char character = (char) charHuechar;
+        return character;
+
+}
