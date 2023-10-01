7cb6cbe17a463ffaa605faf1ca1b164502cb1813
BWbear1 <blackwhitebear1@gmail.com>
2023-10-01 13:47:54 +0200

    T_2_2_9 c 2ой штукой через умную штуку

--- src/T_2_2_9.java
+++ src/T_2_2_9.java
@@ -1,12 +1,18 @@
 public class T_2_2_9 {
     public static void main(String[] args) {
     }
-
-    {
-        public static boolean isPowerOfTwo ( int value){
-        int B = Math.abs(value);
-        int C = B - 1;
-        return ((B & C) == 0) && (B != 0);
-    }
-    }
 }
+
+
+    public static boolean isPowerOfTwo(int value) {
+        int b = Math.abs(value);
+        int c = b - 1;
+        return ((b & c) == 0) && (c != 0);
+    }
+
+    public static boolean isPowerOfTwo(int value) {
+        return Integer.bitCount(Math.abs(value)) == 1;
+    }
+
+
+
