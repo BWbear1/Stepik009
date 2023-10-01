725af867759e42887e6eb782d8ff9bfaf675e2a5
BWbear1 <blackwhitebear1@gmail.com>
2023-10-01 13:20:42 +0200

    T_2_2_2_v1.01

--- src/T_2_2_2.java
+++ src/T_2_2_2.java
@@ -2,8 +2,6 @@ public class T_2_2_2 {
     public static void main(String[] args) {
     }
 
-    char c = '\\';
-    int charValue = c;
-    int charHuechar = charValue+a;
-    char character = (char) charHuechar;
-        return character;
+    public static char charExpression(int a) {
+        return ((char) ('\\'+a));
+    }
