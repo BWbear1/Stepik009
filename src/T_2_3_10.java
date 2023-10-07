5adab1483b2bc9dba619930898be788b034fb4cf
BWbear1 <blackwhitebear1@gmail.com>
2023-10-07 17:45:18 +0200

    T_2_3_10 переназвал штуки

--- src/T_2_3_10.java
+++ src/T_2_3_10.java
@@ -4,7 +4,7 @@ public class T_2_3_10 {
 }
 
     public static boolean isPalindrome(String text) {
-        String editedText = text.replaceAll("\\W|\\s", "");
-        String sb = new StringBuilder(editedText).reverse().toString();
-        return editedText.equalsIgnoreCase(sb);
+        String cleanedString = text.replaceAll("\\W|\\s", "");
+        String reversedString  = new StringBuilder(cleanedString).reverse().toString();
+        return cleanedString.equalsIgnoreCase(reversedString );
     }
