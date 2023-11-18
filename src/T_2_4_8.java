2fea8701bbe94180649c019839cf3957c6839b52
BWbear1 <blackwhitebear1@gmail.com>
2023-11-18 16:18:57 +0200

    T_2_4_9 v2.0

--- src/T_2_4_8.java
+++ src/T_2_4_8.java
@@ -4,20 +4,20 @@ public class T_2_4_8 {
     public static void main(String[] args) {
         int VALUE = 0;
         BigInteger valueBigInteger = BigInteger.valueOf(VALUE);
-        BigInteger Factorial = BigInteger.valueOf(1);
+        BigInteger factorial = BigInteger.valueOf(1);
             for (BigInteger i = BigInteger.ONE; i.compareTo(valueBigInteger.add(BigInteger.ONE)) < 0; i = i.add(BigInteger.ONE)) {
-                Factorial = Factorial.multiply(i);
+                factorial = factorial.multiply(i);
         }
-        System.out.println(Factorial);
+        System.out.println(factorial);
     }
 }
 
 // В степиковставлялка
 //    public static BigInteger factorial(int value) {
 //        BigInteger valueBigInteger = BigInteger.valueOf(value);
-//        BigInteger Factorial = BigInteger.valueOf(1);
+//        BigInteger factorial = BigInteger.valueOf(1);
 //        for (BigInteger i = BigInteger.ONE; i.compareTo(valueBigInteger.add(BigInteger.ONE)) < 0; i = i.add(BigInteger.ONE)) {
-//            Factorial = Factorial.multiply(i);
+//            factorial = factorial.multiply(i);
 //        }
-//        return Factorial;
+//        return factorial;
 //    }
