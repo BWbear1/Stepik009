200f942ee61d7a76aab5f5f6b71cd500cd92e480
BWbear1 <blackwhitebear1@gmail.com>
2023-11-26 11:37:56 +0200

    T_2_4_9 v3.0

--- src/T_2_4_9.java
+++ src/T_2_4_9.java
@@ -2,63 +2,49 @@ import java.util.Arrays;
 
 public class T_2_4_9 {
     public static void main(String[] args) {
-        int[] a1 = new int[]{6};
+        int[] a1 = new int[]{2, 6};
         int[] a2 = new int[]{1, 3, 5};
         int[] result = new int[a1.length + a2.length];
+        int A = 0;
+        int B = 0;
+        int endA1 = a1.length;
+        int endA2 = a2.length;
 
-        for (int i = 0, A = 0, B = 0; i < result.length; i++) {
-            if (B==a2.length) {
+        for (int i = 0; i < result.length; i++) {
+            if (A < endA1 && (B >= endA2 || a1[A] < a2[B])) {
                 result[A + B] = a1[A];
                 A++;
-                continue;
-            }
-            if (A==a1.length)
-            {
-                result[A + B] = a2[B];
-                B++;
-                continue;
-            }
-            if (a1[A] > a2[B]) {
-                result[A + B] = a2[B];
-                B++;
-            }
-            else {
+            } else {
                 {
-                    result[A + B] = a1[A];
-                A++;
-            }
+                    result[A + B] = a2[B];
+                    B++;
+                }
 
-        }}
+            }
+        }
 
         System.out.println(Arrays.toString(result));
     }
 }
-// В степиковставлялка
+
+    // В степиковставлялка
 //    public static int[] mergeArrays(int[] a1, int[] a2) {
 //        int[] result = new int[a1.length + a2.length];
-//
-//        for (int i = 0, A = 0, B = 0; i < result.length; i++) {
-//            if (B==a2.length) {
+//        int A = 0;
+//        int B = 0;
+//        int endA1 = a1.length;
+//        int endA2 = a2.length;
+//        for (int i = 0; i < result.length; i++) {
+//            if (A < endA1 && (B >= endA2 || a1[A] < a2[B])) {
 //                result[A + B] = a1[A];
 //                A++;
-//                continue;
-//            }
-//            if (A==a1.length)
-//            {
-//                result[A + B] = a2[B];
-//                B++;
-//                continue;
-//            }
-//            if (a1[A] > a2[B]) {
-//                result[A + B] = a2[B];
-//                B++;
-//            }
-//            else {
+//            } else {
 //                {
-//                    result[A + B] = a1[A];
-//                    A++;
+//                    result[A + B] = a2[B];
+//                    B++;
 //                }
 //
-//            }}
+//            }
+//        }
 //        return result; // your implementation here
 //    }
