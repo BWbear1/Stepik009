d07a8bebb9753854bc833b6510d2e4233277c4bb
BWbear1 <blackwhitebear1@gmail.com>
2023-11-26 11:39:38 +0200

    T_2_4_9 v3.01

--- src/T_2_4_9.java
+++ src/T_2_4_9.java
@@ -5,19 +5,19 @@ public class T_2_4_9 {
         int[] a1 = new int[]{2, 6};
         int[] a2 = new int[]{1, 3, 5};
         int[] result = new int[a1.length + a2.length];
-        int A = 0;
-        int B = 0;
+        int a = 0;
+        int b = 0;
         int endA1 = a1.length;
         int endA2 = a2.length;
 
         for (int i = 0; i < result.length; i++) {
-            if (A < endA1 && (B >= endA2 || a1[A] < a2[B])) {
-                result[A + B] = a1[A];
-                A++;
+            if (a < endA1 && (b >= endA2 || a1[a] < a2[b])) {
+                result[a + b] = a1[a];
+                a++;
             } else {
                 {
-                    result[A + B] = a2[B];
-                    B++;
+                    result[a + b] = a2[b];
+                    b++;
                 }
 
             }
