e188554d4280a16ddcc0148939fd43aeee826c51
BWbear1 <blackwhitebear1@gmail.com>
2024-04-03 21:07:25 +0200

    Все_сломать_к_чертям_часть3.3

--- src/T_2_3_10.java
+++ src/T_2_3_10.java
@@ -1,10 +1,10 @@
 public class T_2_3_10 {
     public static void main(String[] args) {
     }
-}
-
 //    public static boolean isPalindrome(String text) {
 //        String cleanedString = text.replaceAll("\\W|\\s", "");
-//        String reversedString  = new StringBuilder(cleanedString).reverse().toString();
-//        return cleanedString.equalsIgnoreCase(reversedString );
+//        String reversedString = new StringBuilder(cleanedString).reverse().toString();
+//        return cleanedString.equalsIgnoreCase(reversedString);
 //    }
+
+}
