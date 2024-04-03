592ea5072cd7d45592b8ea55aee33542d8f478dc
BWbear1 <blackwhitebear1@gmail.com>
2024-04-03 21:11:06 +0200

    Все_сломать_к_чертям_часть3.5

--- src/T_2_2_2.java
+++ src/T_2_2_2.java
@@ -1,7 +1,9 @@
 public class T_2_2_2 {
     public static void main(String[] args) {
-    }}
+    }
 
 //    public static char charExpression(int a) {
 //        return (char) ('\\' + a);
 //    }
+
+}
