8faeccc63cd5c65e813a08e487066a0b13d49c86
BWbear1 <blackwhitebear1@gmail.com>
2024-04-05 18:53:55 +0200

    Все_сломать_к_чертям_часть3.8

--- src/T_3_3_12.java
+++ src/T_3_3_12.java
@@ -1,40 +1,43 @@
 public class T_3_3_12 {
     public static void main(String[] args) {
     }
-    }
 
-// тыкал на обум ибо я ебал писать робота ябучего
-    public static void moveRobot(Robot robot, int toX, int toY) {
 
-        if ( robot.getY()<toY)
-        {
-            while (robot.getDirection()!=Direction.UP)
-            {robot.turnRight();}
-            while (robot.getY()!=toY)
-            {robot.stepForward();}
-        }
-        if ( robot.getY()>toY)
-        {
-            while (robot.getDirection()!=Direction.DOWN)
-            {robot.turnRight();}
-            while (robot.getY()!=toY)
-            {robot.stepForward();}
-        }
-        if ( robot.getX()<toX)
-        {
-            while (robot.getDirection()!=Direction.RIGHT)
-            {robot.turnRight();}
-            while (robot.getX()!=toX)
-            {robot.stepForward();}
-        }
-        if ( robot.getX()>toX)
-        {
-            while (robot.getDirection()!=Direction.LEFT)
-            {robot.turnRight();}
-            while (robot.getX()!=toX)
-            {robot.stepForward();}
-        }
-    }
+//    public static void moveRobot(Robot robot, int toX, int toY) {
+//
+//        if (robot.getY() < toY) {
+//            while (robot.getDirection() != Direction.UP) {
+//                robot.turnRight();
+//            }
+//            while (robot.getY() != toY) {
+//                robot.stepForward();
+//            }
+//        }
+//        if (robot.getY() > toY) {
+//            while (robot.getDirection() != Direction.DOWN) {
+//                robot.turnRight();
+//            }
+//            while (robot.getY() != toY) {
+//                robot.stepForward();
+//            }
+//        }
+//        if (robot.getX() < toX) {
+//            while (robot.getDirection() != Direction.RIGHT) {
+//                robot.turnRight();
+//            }
+//            while (robot.getX() != toX) {
+//                robot.stepForward();
+//            }
+//        }
+//        if (robot.getX() > toX) {
+//            while (robot.getDirection() != Direction.LEFT) {
+//                robot.turnRight();
+//            }
+//            while (robot.getX() != toX) {
+//                robot.stepForward();
+//            }
+//        }
+}
 
 
 
